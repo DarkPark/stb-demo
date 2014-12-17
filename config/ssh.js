ad6c4fd8d3d2afbbb4c6acfb7c90034c0dfc2632
Stanislav Kalashnik <sk@infomir.eu>
2014-12-17 16:59:20 +0200

    disable ws logger and add SSH config

---
+++ config/ssh.js
@@ -0,0 +1,38 @@
+/**
+ * Remote STB device connection configuration.
+ *
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public export
+module.exports = {
+	// turn on/off
+	active: true,
+
+	// default config
+	defaults: {
+		host: '192.168.1.63',
+		port: 22,
+		user: 'root',
+		pass: '930920',
+		key:  '',
+		url:  '',
+		exec: 'killall stbapp; /usr/share/qt-4.6.0/stbapp -qws -display directfb http://%s:%s/%s'
+	},
+
+	// set of named configs
+	profiles: {
+		// extends the default config
+		develop: {
+			url: 'develop/'
+		},
+
+		// extends the default config
+		release: {
+			url: 'release/'
+		}
+	}
+};
