f8e157ec9f3623e533521b16ab070f929590ce7a
Stanislav Kalashnik <sk@infomir.eu>
2014-12-22 18:54:21 +0200

    clean

--- app/js/pages/help.js
+++ app/js/pages/help.js
@@ -10,22 +10,11 @@ var id     = 'pageHelp',
 	node   = document.getElementById(id),
 	Page   = require('stb/ui/page'),
 	Button = require('stb/ui/button'),
-	Panel  = require('stb/ui/panel'),
 	page   = new Page({$node: node}),
-	router = require('stb/router'),
-	keys   = require('stb/keys');
-
-
-page.addListener('show', function load () {
-	// initial active component
-	//page.back.focus();
-});
+	router = require('stb/router');
 
 
 page.addListener('load', function load () {
-	var body = new Panel(),
-		header, button;
-
 	page.add(
 		page.back = new Button({
 			value: 'go back',
@@ -36,35 +25,12 @@ page.addListener('load', function load () {
 			}
 		})
 	);
-
-	return;
-
-	page.add(body);
-
-	body.add(new Panel({$node: document.getElementById('pageHelpTitle')}));
-
-	body.add(header = new Panel());
-
-
-
-	button.focus();
 });
 
 
-//page.addListener('show', function show ( event ) {
-//	debug.info(event.data);
-//});
-
-
-page.addListener('keydown', function keydown ( event ) {
-//	switch ( event.code ) {
-//		case keys.ok:
-//			require('./base').show();
-//			break;
-//		case keys.exit:
-//			page.hide();
-//			break;
-//	}
+page.addListener('show', function load () {
+	// initial active component
+	page.back.focus();
 });
 
 
