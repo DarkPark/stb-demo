76d5a6b3b844d129ae3afe97082b2bfab5508f31
Stanislav Kalashnik <sk@infomir.eu>
2015-07-02 15:17:25 +0300

    add preloader use

--- app/js/tabs/main.button.js
+++ app/js/tabs/main.button.js
@@ -7,12 +7,18 @@
 
 'use strict';
 
-var Button = require('../stb/ui/button'),
-	Panel  = require('../stb/ui/panel'),
-	panel  = new Panel({
+var Button    = require('../stb/ui/button'),
+	Panel     = require('../stb/ui/panel'),
+	preloader = require('../stb/preloader'),
+	panel     = new Panel({
 		$node: document.getElementById('pageMainTabButton'),
 		visible: false
 	});
+
+
+preloader.addListener('done', function () {
+	debug.log('ready');
+});
 
 
 panel.add(
@@ -20,7 +26,30 @@ panel.add(
 		$node: document.getElementById('pageMainTabButtonSimple'),
 		children: [
 			new Button({
-				value: 'press me'
+				value: 'preload images',
+				events: {
+					click: function () {
+						debug.log('click');
+
+						preloader.add([
+							'http://pic.uuhy.com/uploads/2011/09/01/Painting-Of-Nature.png',
+							'https://perishablepress.com/wp/wp-content/themes/wire/img/jeff-starr.jpg',
+							{url: 'http://www.phpied.com/files/reflow/dyna1.png', group:'qwe'},
+							{url: 'http://www.phpied.com/files/reflow/dyna3.png', group:'qwe'},
+							'http://www.phpied.com/files/reflow/render.pn'
+						]);
+					}
+				}
+			}),
+			new Button({
+				value: 'show heavy image',
+				events: {
+					click: function () {
+						debug.log('click');
+
+						panel.$node.style.background = 'url("http://pic.uuhy.com/uploads/2011/09/01/Painting-Of-Nature.png") center center';
+					}
+				}
 			})
 		]
 	}),
