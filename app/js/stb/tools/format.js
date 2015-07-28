d10202b7ad8f8c577fe603a185abfd13e2148840
Stanislav Kalashnik <sk@infomir.eu>
2015-07-28 14:04:10 +0300

    add missing files

---
+++ app/js/stb/tools/format.js
@@ -0,0 +1,34 @@
+/**
+ * @module stb/tools
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+
+/**
+ * Do string substitution according to the given format.
+ * http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+ *
+ * @param {string} format string substitution format
+ *
+ * @return {string} result data
+ *
+ * @example
+ * format('This is a {0}', 'cat');
+ * format('This is a {0} and a {1}', 'cat', 'dog');
+ * format('This is a {0} and a {1} and another {0}', 'cat', 'dog');
+ */
+module.exports = function ( format ) {
+	var args = Array.prototype.slice.call(arguments, 1),
+		expr = /{(\d+)}/g;
+
+	if ( DEBUG ) {
+		if ( !expr.test(format) ) { throw new Error(__filename + ': format string does not have substitutions'); }
+	}
+
+	return format.replace(expr, function ( match, number ) {
+		return args[number] !== undefined ? args[number] : match;
+	});
+};
---
+++ app/js/stb/tools/referrer.js
@@ -0,0 +1,38 @@
+/**
+ * @module stb/tools
+ * @author Stanislav Kalashnik <sk@infomir.eu>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var parseQuery = require('./parse.query');
+
+
+/**
+ * Determine application referrer.
+ * If no referrer - return false.
+ *
+ * @return {string|boolean} referrer url or false
+ *
+ * @example
+ * location.href = referrer() || 'http://google.com';
+ */
+module.exports = function () {
+	var queryParams = parseQuery(location.search.substring(1));
+
+	if ( queryParams.referrer ) {
+		// referrer in GET
+		return decodeURIComponent(queryParams.referrer);
+	}
+
+	if ( document.referrer ) {
+		// if in app was used location.reload method, document.referrer === app link, and must return false
+		if ( location.href.split('#')[0] === document.referrer ) {
+			return false;
+		}
+		return document.referrer;
+	}
+
+	return false;
+};
